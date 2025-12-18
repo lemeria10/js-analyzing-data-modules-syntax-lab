5a5640e10c213ade0f543e03c9ad81d46065f0b4
Lemeria <nickylemeria10@gmail.com>
2025-12-18 09:03:41 +0300

    Remove nested git and add files

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+require('datejs');
+
+
+function combineUsers(...args) {
+  let combinedObject = {
+    users: []
+  };
+
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users.push(...args[i]);
+  }
+
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+console.log(
+  combineUsers(['nick'], ['john'], ['mary'])
+);
+
 
 
 
